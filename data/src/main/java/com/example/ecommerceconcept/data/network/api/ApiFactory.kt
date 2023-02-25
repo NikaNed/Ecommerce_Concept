28a1826d33bc389ecb29815055a698ac051b1569
NikaNed <babenkova.nika@ya.ru>
2023-02-25 12:34:59 +0300

    Edit viewModels

--- data/src/main/java/com/example/ecommerceconcept/data/network/api/ApiFactory.kt
+++ data/src/main/java/com/example/ecommerceconcept/data/network/api/ApiFactory.kt
@@ -2,8 +2,8 @@ package com.example.ecommerceconcept.data.network.api
 
 
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 object ApiFactory {
 
@@ -12,9 +12,7 @@ object ApiFactory {
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
-        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .build()
 
-    val apiService: ApiService =
-        retrofit.create(ApiService::class.java)
+    val apiService: ApiService = retrofit.create()
     }
